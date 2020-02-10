a3f997d6bc2df5c227540d77a0668227ba4006cc
Make <make_2019@126.com>
2020-02-10 14:58:49 +0800

    使用selenium登录获取COOKIE之后转交给requests进行操作

--- dome.py
+++ dome.py
@@ -9,7 +9,7 @@ def log_cookie():
     dl = driver.find_element_by_xpath("//span[@class='abut showLogin']").click()
     sleep(1)
     driver.find_element_by_xpath('//*[@id="loginname"]').send_keys('17716550759')
-    driver.find_element_by_xpath('//*[@id="password"]').send_keys('xxxxxx')
+    driver.find_element_by_xpath('//*[@id="password"]').send_keys('ying3690')
     driver.find_element_by_xpath('//*[@id="login_btn"]').click()
     sleep(1)
     if driver.find_element_by_xpath('/html/body/div[1]/div[1]/div/div[3]/ul/li[1]/a').text == '马可':
@@ -32,10 +32,11 @@ def user_data():
         cookie = json.load(f)
         for i in cookie:
             cookie_dict[i['name']] = i['value']
-    r = session.get('https://i.51job.com/resume/standard_resume.php', cookies=cookie_dict)
-    r.encoding = 'gbk'
+    r = session.get('https://search.51job.com/list/010000,000000,0000,00,9,99,%25E8%25BD%25AF%25E4%25BB%25B6%25E6%25B5%258B%25E8%25AF%2595,2,1.html?lang=c&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&ord_field=0&dibiaoid=0&line=&welfare=', cookies=cookie_dict)
+    r.encoding = 'utf-8'
     print(r.text)
 
 
 if __name__ == '__main__':
+    # log_cookie()
     user_data()
