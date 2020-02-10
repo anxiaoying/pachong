4e6dfd0ab9d6b3cae4967c15c807ca9f2a0779af
Make <make_2019@126.com>
2020-02-10 15:16:24 +0800

    获取软件测试行业的招聘信息

--- dome.py
+++ dome.py
@@ -25,7 +25,7 @@ def log_cookie():
 def user_data():
     session = requests.session()
     session.headers ={
-        "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Mobile/15A372 Safari/604.1"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.88 Safari/537.36"
     }
     cookie_dict = dict()
     with open('cookie.ini','r') as f:
@@ -33,7 +33,7 @@ def user_data():
         for i in cookie:
             cookie_dict[i['name']] = i['value']
     r = session.get('https://search.51job.com/list/010000,000000,0000,00,9,99,%25E8%25BD%25AF%25E4%25BB%25B6%25E6%25B5%258B%25E8%25AF%2595,2,1.html?lang=c&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&ord_field=0&dibiaoid=0&line=&welfare=', cookies=cookie_dict)
-    r.encoding = 'utf-8'
+    r.encoding = 'gbk'
     print(r.text)
 
 
