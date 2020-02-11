a7b3516a314c8bd2b5b43440c573875ec2f54de9
Make <make_2019@126.com>
2020-02-11 14:23:46 +0800

    第二次改版，爬下来的数据写入数据库中

--- demo.py
+++ demo.py
@@ -2,8 +2,9 @@ import json
 from time import sleep
 import requests
 from selenium import webdriver
-
-
+from lxml import etree
+from peewee_text import School
+import datetime
 def log_cookie():
     driver = webdriver.Firefox()
     driver.get('https://www.51job.com/')
@@ -36,7 +37,28 @@ def user_data():
             cookie_dict[i['name']] = i['value']
     r = session.get('https://search.51job.com/list/010000,000000,0000,00,9,99,%25E8%25BD%25AF%25E4%25BB%25B6%25E6%25B5%258B%25E8%25AF%2595,2,1.html?lang=c&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&ord_field=0&dibiaoid=0&line=&welfare=', cookies=cookie_dict)
     r.encoding = 'gbk'
-    print(r.text)
+    tree = etree.HTML(r.text)
+    xpaths = tree.xpath('//div[@class="el"]')
+    zw = tree.xpath( '//div[@class="el"]/p/span/a/@title')
+    gs = tree.xpath( '//div[@class="el"]/span[@class="t2"]/a/text()')
+    dd = tree.xpath( '//div[@class="el"]/span[@class="t3"]/text()')
+    xz = tree.xpath( '//div[@class="el"]/span[@class="t4"]' )
+    sj = tree.xpath( '//div[@class="el"]/span[@class="t5"]/text()')
+
+
+    for i in range(len(zw)):
+
+        if xz[i].text != None:
+            print('当前职位：%s，公司名称：%s,公司地址：%s,薪资：%s,发布时间%s'%(zw[i],gs[i],dd[i],xz[i].text,sj[i]))
+            School.insert( job_name = zw[i], company = gs[i], pay = xz[i].text, site = dd[i], time = sj[i],
+                           posi = '职位信息' ).execute()
+        else:
+
+            print( '当前职位：%s，公司名称：%s,公司地址：%s,薪资：%s,发布时间%s' % (zw[i], gs[i], dd[i],'薪资未定', sj[i]) )
+            School.insert( job_name = zw[i], company = gs[i], pay = '薪资未定', site = dd[i], time = sj[i],
+                           posi = '职位信息' ).execute()
+
+
 
 
 if __name__ == '__main__':
---
+++ peewee_text.py
@@ -0,0 +1,29 @@
+from peewee import *
+
+db = MySQLDatabase(host='47.100.20.184',
+                   user='zp_data',
+                   passwd='23raBTr7M4GWiZzm',
+                   database='zp_data',
+                   charset='utf8',
+                   port=3306)
+
+
+
+class School(Model):
+    job_name = CharField()   #职位名称
+    company = CharField()    #公司名称
+    pay = CharField()        #薪资
+    site = CharField()       #公司地址
+    time = CharField()    #发布时间
+    posi = CharField()       #职位信息
+    class Meta:
+        database = db
+
+if __name__ == '__main__':
+    School.create_table()
+    # School.insert(job_name='职位名称', company='公司名称', pay='薪资', site='公司地址', time='发布时间', posi='职位信息').execute()
+
+
+
+
+
